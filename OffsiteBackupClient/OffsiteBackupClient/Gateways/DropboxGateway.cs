1be4f3d94ba0ccbaa2d1041bfd42de506e52a3ca
Barend Erasmus <developersworkspace@gmail.com>
2017-09-18 08:05:09 +0200

    Clean up

--- OffsiteBackupClient/OffsiteBackupClient/Gateways/DropboxGateway.cs
+++ OffsiteBackupClient/OffsiteBackupClient/Gateways/DropboxGateway.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OffsiteBackupClient.Gateways
 {
--- OffsiteBackupClient/OffsiteBackupClient/Gateways/IGateway.cs
+++ OffsiteBackupClient/OffsiteBackupClient/Gateways/IGateway.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-
-namespace OffsiteBackupClient.Gateways
+﻿namespace OffsiteBackupClient.Gateways
 {
     public interface IGateway
     {
